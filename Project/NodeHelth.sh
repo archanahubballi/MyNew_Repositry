a14c77d7b9bc5a65099555bd0f9aa2d611bd0dba
archanahubballi <ahubballi707@gmail.com>
2023-12-09 12:37:54 +0530

    Nodehealth.sh file added

---
+++ Project/NodeHelth.sh
@@ -0,0 +1,20 @@
+ #!/bin/bash
+
+ ######################
+ # Author: Archana
+ # Date: 09-12-2023
+ # This scripts outputs the node health
+ # version: v1
+ 
+ ######################
+ 
+ echo "Print the disk space"
+ df -h
+
+ echo "Print the Memory"
+ free -g
+
+ echo "Print the CPU"
+ nproc
+
+
