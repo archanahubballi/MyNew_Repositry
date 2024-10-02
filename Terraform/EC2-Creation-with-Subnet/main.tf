ab81bbdc65e737d95b5bbc5524388f5ae16e906f
archanahubballi <125686980+archanahubballi@users.noreply.github.com>
2024-10-02 08:03:32 +0000

    Modified the variables.tf file

--- Terraform/EC2-Creation-with-Subnet/main.tf
+++ Terraform/EC2-Creation-with-Subnet/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "Test" {
-  ami = "var.ami_value"
-  instance_type = "var.instance_type_value"
-  subnet_id = "var.subnet_id_value"
+resource "aws_instance" "example" {
+    ami = var.ami_value
+    instance_type = var.instance_type_value
+    subnet_id = var.subnet_id_value
 }
