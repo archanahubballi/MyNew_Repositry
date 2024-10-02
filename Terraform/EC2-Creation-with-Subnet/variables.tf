88dacb2ace2fc6b23f26917b4495e59d5811d781
archanahubballi <125686980+archanahubballi@users.noreply.github.com>
2024-10-02 07:14:04 +0000

    moved all the files to EC2-Creation-with-Subnet folder

---
+++ Terraform/EC2-Creation-with-Subnet/variables.tf
@@ -0,0 +1,14 @@
+variable "ami_value" {
+  description = "value for the ami"
+  type = string
+}
+
+variable "instance_type_value" {
+  description = "value for instance_type"
+  type = string
+}
+
+variable "subnet_id_value" {
+  description = "value for subnet_id"
+  type = string
+}
